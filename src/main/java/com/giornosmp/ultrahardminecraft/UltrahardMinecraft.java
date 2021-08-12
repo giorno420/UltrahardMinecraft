fbbf8cde9223c06f756ac15bcaeba85efc0a23da
giorno420 <74859235+giorno420@users.noreply.github.com>
2021-08-12 13:32:23 +0100

    difficulty is always at hard

--- src/main/java/com/giornosmp/ultrahardminecraft/UltrahardMinecraft.java
+++ src/main/java/com/giornosmp/ultrahardminecraft/UltrahardMinecraft.java
@@ -8,51 +8,57 @@ import com.giornosmp.ultrahardminecraft.HostileMobs.*;
 import com.giornosmp.ultrahardminecraft.PassiveMobs.PissEndermen;
 import com.giornosmp.ultrahardminecraft.PassiveMobs.PissIronGolems;
 import com.giornosmp.ultrahardminecraft.PassiveMobs.ReduceFoodSources;
-import org.bukkit.Server;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.ChatColor;
 
 
 public final class UltrahardMinecraft extends JavaPlugin {
 
+    private final String pluginPrefix = ChatColor.WHITE + "[" + ChatColor.DARK_RED + "Ultrahard Minecraft" + ChatColor.WHITE + "] ";
+
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new Creepers(), this);
+        PluginManager pluginmanager = getServer().getPluginManager();
+
+        pluginmanager.registerEvents(new Creepers(), this);
         // charges all creepers
 
-        getServer().getPluginManager().registerEvents(new UndeadMobs(), this);
+        pluginmanager.registerEvents(new UndeadMobs(), this);
         // gives all mobs better shit
 
-        getServer().getPluginManager().registerEvents(new ReduceFoodSources(), this);
+        pluginmanager.registerEvents(new ReduceFoodSources(), this);
         // halves all livestock and fish
 
-        getServer().getPluginManager().registerEvents(new MobHints(), this);
+        pluginmanager.registerEvents(new MobHints(), this);
         // gives hints to the player when the player encounters them
 
-        getServer().getPluginManager().registerEvents(new PissIronGolems(), this);
+        pluginmanager.registerEvents(new PissIronGolems(), this);
         // pisses all iron golems in the vicinity
 
-        getServer().getPluginManager().registerEvents(new ExplodingBeds(), this);
+        pluginmanager.registerEvents(new ExplodingBeds(), this);
         // random chance of your bed just fucking exploding
 
-        getServer().getPluginManager().registerEvents(new PissEndermen(), this);
+        pluginmanager.registerEvents(new PissEndermen(), this);
         // pisses all endermen in the vicinity
 
-        getServer().getPluginManager().registerEvents(new InstantlyExplodingCreepers(), this);
+        pluginmanager.registerEvents(new InstantlyExplodingCreepers(), this);
         // makes all creepers instantly explode in proximity
 
-        getServer().getPluginManager().registerEvents(new Lava(), this);
+        pluginmanager.registerEvents(new Lava(), this);
         // fix entire event listener later
 
-        getServer().getPluginManager().registerEvents(new SkeletonTypeMobs(), this);
+        pluginmanager.registerEvents(new SkeletonTypeMobs(), this);
         // gives you a withering effect when attacked by skeletons and strays
 
-        getServer().getPluginManager().registerEvents(new ToolsRequireExperience(), this);
+        pluginmanager.registerEvents(new ToolsRequireExperience(), this);
         // all iron and diamond tools require experience levels to be used
 
-        getServer().getPluginManager().registerEvents(new AttackOnTitan(), this);
+        pluginmanager.registerEvents(new AttackOnTitan(), this);
         // a zombie has a 1 in 50 chance of dying and becoming a titan (giant) I'm gonna work on the zombie AI soon fuck you
 
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "difficulty hard"); // ligma balls
 
     }
 
@@ -60,4 +66,5 @@ public final class UltrahardMinecraft extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
+    
 }
